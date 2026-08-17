444f0cb259fa005e42476cd15743ed7b49bcf870
tonoysharma <tonoyshaema58@gmail.com>
2026-08-17 15:58:47 +0600

    Work on Tonoy branch

--- techbasket_backend/index.js
+++ techbasket_backend/index.js
@@ -1,35 +1,27 @@
-import type { Application, Request, Response, NextFunction } from 'express';
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-
 // Load environment variables
 dotenv.config();
-
-const app: Application = express();
-const PORT: number = parseInt(process.env.PORT || '5000', 10);
-
+const app = express();
+const PORT = parseInt(process.env.PORT || '5000', 10);
 // Middleware
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 // Basic Route
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req, res) => {
     res.json({ message: 'Server is running' });
 });
-
 // Global Error Handler (Middleware order matters)
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: 'Something went wrong!' });
 });
-
 // Start Server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
-
-export default app;   
+exports.default = app;
